89d9ae8a23ac3d3b13f1ce3a583d1e3dbb9f8be8
kkava <kavace@gmail.com>
2015-04-26 10:45:21 +0200

    Create de-en_dict.cc
    1. This file is required - you must request it from 
    
    http://www1.dict.cc/translation_file_request.php?l=e
    
    2. Replace this file with the downloaded one (keeping the name "de-en_dict.cc"
    
    3. Run dictSplit.py
        -This produces a subdirectory dictionary structure which dict.php then uses to look up words

---
+++ de-en_dict.cc
@@ -0,0 +1,8 @@
+1. This file is required - you must request it from 
+
+http://www1.dict.cc/translation_file_request.php?l=e
+
+2. Replace this file with the downloaded one (keeping the name "de-en_dict.cc"
+
+3. Run dictSplit.py
+    -This produces a subdirectory dictionary structure which dict.php then uses to look up words
